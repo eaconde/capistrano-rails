5d1643f091037fa947cdb4a1f27bf2959fa79a15
Eric Conde <condeeric@live.com>
2015-10-01 01:18:57 +0800

    updated proj config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 # set path to application
-# app_dir = File.expand_path("../..", __FILE__)
+app_dir = File.expand_path("../..", __FILE__)
 # shared_dir = "#{app_dir}/shared"
 shared_dir = "/home/deployer/apps/capistrano-rails/shared"
 working_directory app_dir
