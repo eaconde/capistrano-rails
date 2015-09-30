1b830b233e0894111d87c254f2b8446793ffbc35
Eric Conde <condeeric@live.com>
2015-10-01 01:00:53 +0800

    mod unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,7 @@
 # set path to application
-# app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "/home/deployer/apps/capistrano-rails/shared"
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+# shared_dir = "/home/deployer/apps/capistrano-rails/shared"
 working_directory app_dir
 
 
@@ -10,7 +11,7 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "#{shared_dir}/tmp/sockets/unicorn.sock", :backlog => 64
 
 # Logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
